a7c7b9e8e003f198bf8b6ed11becc1d498e59d76
CarlosSantiago Nevett <csnevett@gmail.com>
2021-03-22 18:10:29 -0400

    completed episodes & show tests

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,28 +5,98 @@ import userEvent from '@testing-library/user-event';
 import Show from './../Show';
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  name: 'Stranger Things',
+  image: {
+    medium:
+      'https://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg',
+    original:
+      'https://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg',
+  },
+  summary:
+    "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.",
+  seasons: [
+    {
+      id: 0,
+      name: 'Season 1',
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: 'Season 2',
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: 'Season 3',
+      episodes: [],
+    },
+    {
+      id: 3,
+      name: 'Season 4',
+      episodes: [],
+    },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test('renders testShow and no selected Season without errors', () => {
+  render(<Show show={testShow} selectedSeason={'none'} />);
 });
 
 test('renders Loading component when prop show is null', () => {
+  render(<Show show={null} />);
+
+  const loading = screen.queryByTestId(/loading-container/i);
+
+  expect(loading).toBeInTheDocument();
+  expect(loading).toBeTruthy();
+  expect(loading).toHaveTextContent(/fetching data.../i);
 });
 
-test('renders same number of options seasons are passed in', ()=>{
+test('renders same number of options seasons are passed in', () => {
+  //Arrange
+  render(<Show show={testShow} selectedSeason={'none'} />);
+
+  const seasonOption = screen.queryAllByTestId(/season-option/i);
+
+  expect(seasonOption.length).toEqual(testShow.seasons.length);
 });
 
 test('handleSelect is called when an season is selected', () => {
+  //Arrange
+  const mockHandleSelect = jest.fn();
+  render(
+    <Show
+      show={testShow}
+      selectedSeason={'none'}
+      handleSelect={mockHandleSelect}
+    />
+  );
+  //Act
+  const seasonsSelect = screen.queryByTestId(/seasons-select/i);
+  userEvent.selectOptions(seasonsSelect, '1');
+  userEvent.selectOptions(seasonsSelect, '0');
+
+  //Assert
+  expect(mockHandleSelect).toHaveBeenCalled();
+  expect(mockHandleSelect).toHaveBeenCalledTimes(2);
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+  //Arrange
+  const { rerender } = render(<Show show={testShow} selectedSeason={'none'} />);
+  let episodesContainer = screen.queryByTestId(/episodes-container/i);
+  expect(episodesContainer).toBeNull();
+
+  rerender(<Show show={testShow} selectedSeason={'0'} />);
+  episodesContainer = screen.queryByTestId(/episodes-container/i);
+  expect(episodesContainer).toBeInTheDocument();
+  expect(episodesContainer).not.toBeNull();
 });
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
 //2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
-//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existence)
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
 //6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
